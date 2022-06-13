c1ec6fa100468bf1499fa9c115ff95ebbc5fd3ab
Lily <hayashi.quan@gmail.com>
2022-06-13 22:59:48 +0200

    fix: logo won't appear & socialMedia has no fix width & move head meta tags in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,28 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="description" content="Scarlet Restaurant" />
+        <link rel="icon" href="/logo192.ico" />
+
+        <meta name="theme-color" content="#4f46e5"/>
+
+        <meta name="keywords" content="restaurant, scarlet, showcase site" />
+        <meta name= "author" content= "Lily B" />
+
+        <meta property="og:title" content="Scarlet Restaurant"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Scarlet is a modern restaurant located in Manhattan."/>
+        <meta property="og:site_name" content="Scarlet Restaurant"/>
+        <meta property="og:image" content=""/>
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="590" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url" content=""/>
+        
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image:alt" content=""/>
+
+
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"/>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import Logo from '../components/Logo/Logo'
+import Logo from '../components/logo/Logo'
 import Slider from '../components/slider/Slider'
 import SocialMedia from '../components/socialMedia/SocialMedia'
 
@@ -12,26 +12,6 @@ export default function Home() {
 
       <Head>
         <title>The Scarlet</title>
-        <meta name="description" content="Scarlet Restaurant" />
-        <link rel="icon" href="/logo192.ico" />
-
-        <meta name="theme-color" content="#4f46e5"/>
-
-        <meta name="keywords" content="restaurant, scarlet, showcase site" />
-        <meta name= "author" content= "Lily B" />
-
-        <meta property="og:title" content="Scarlet Restaurant"/>
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content="Scarlet is a modern restaurant located in Manhattan."/>
-        <meta property="og:site_name" content="Scarlet Restaurant"/>
-        <meta property="og:image" content=""/>
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="590" />
-        <meta property="og:image:height" content="300" />
-        <meta property="og:url" content=""/>
-        
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:image:alt" content=""/>
       </Head>
 
       <header>
