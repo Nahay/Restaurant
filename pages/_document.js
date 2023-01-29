7c579b150d6a1fcb6455085c6d5451d153d8907d
Lily <hayashi.quan@gmail.com>
2023-01-29 13:39:45 +0100

    fix - og:url

--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="description" content="Scarlet Restaurant" />
-        <link rel="icon" href="/logo/logo192.ico" />
+    return (
+        <Html lang='en'>
+            <Head>
+                <meta name='description' content='Scarlet Restaurant' />
+                <link rel='icon' href='/logo/logo192.ico' />
 
-        <meta name="theme-color" content="#F1BE71"/>
+                <meta name='theme-color' content='#F1BE71' />
 
-        <meta name="keywords" content="restaurant, scarlet, showcase site" />
-        <meta name= "author" content= "Lily B" />
+                <meta name='keywords' content='restaurant, scarlet, showcase site' />
+                <meta name='author' content='Lily B' />
 
-        <meta property="og:title" content="Scarlet Restaurant"/>
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content="Scarlet is a modern restaurant located in Manhattan."/>
-        <meta property="og:site_name" content="Scarlet Restaurant"/>
-        <meta property="og:image" content="https://raw.githubusercontent.com/Nahay/Assets/master/Scarlet/banner.png"/>
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="590" />
-        <meta property="og:image:height" content="300" />
-        <meta property="og:url" content="https://thescarlet.vercel.app/"/>
-        
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:image:alt" content="https://raw.githubusercontent.com/Nahay/Assets/master/Scarlet/banner.png"/>
+                <meta property='og:title' content='Scarlet Restaurant' />
+                <meta property='og:type' content='website' />
+                <meta property='og:description' content='Scarlet is a modern restaurant located in Manhattan.' />
+                <meta property='og:site_name' content='Scarlet Restaurant' />
+                <meta property='og:image' content='https://raw.githubusercontent.com/Nahay/Assets/master/Scarlet/banner.png' />
+                <meta property='og:image:type' content='image/png' />
+                <meta property='og:image:width' content='590' />
+                <meta property='og:image:height' content='300' />
+                <meta property='og:url' content='https://thescarlet.lilybarberou.fr/' />
 
+                <meta name='twitter:card' content='summary_large_image' />
+                <meta name='twitter:image:alt' content='https://raw.githubusercontent.com/Nahay/Assets/master/Scarlet/banner.png' />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"/>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+                <link href='https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap' rel='stylesheet' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
